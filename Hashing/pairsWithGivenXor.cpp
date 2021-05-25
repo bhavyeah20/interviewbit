8573b03a26949761c908832594c1a87300102afe
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2021-05-25 19:19:22 +0530

    grinding

---
+++ Hashing/pairsWithGivenXor.cpp
@@ -0,0 +1,31 @@
+int Solution::solve(vector<int> &A, int B) {
+	int cnt = 0;
+	unordered_map<int, bool> mp;
+	for (int x : A)
+		mp[x] = 1;
+
+	for (int x : A) {
+		if (mp[x ^ B]) {
+			cnt++;
+		}
+	}
+
+	return cnt / 2;
+}
+
+//or
+
+int Solution::solve(vector<int> &A, int B) {
+	unordered_set<int> st;
+	int a = A.size();
+	int cnt = 0;
+	for (int i = 0; i < a; i++)
+	{
+		int xr = A[i] ^ B;
+		if (st.find(xr) == st.end())
+			st.insert(A[i]);
+		else
+			cnt++;
+	}
+	return cnt;
+}
