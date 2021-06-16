3d6c005b88fad68848196ae46e107352cce8d497
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2021-06-16 22:51:27 +0530

    grinding

--- Dynamic Programming/test.cpp
+++ Dynamic Programming/test.cpp
@@ -1,37 +1,12 @@
-#include<bits/stdc++.h>
+#include<iostream>
 
 using namespace std;
 
-unordered_map<string, int> mp;
-
-bool partition(string A, int i, int j) {
-	if (i == A.size())
-		return true;
-
-	for (int k = i; k < A.size(); k++) {
-		if (mp.find(A.substr(i, k - i + 1)) != mp.end() && partition(A, k + 1, j)) {
-			return true;
-		}
-	}
-
-	return false;
-
-}
-
-int Solution::wordBreak(string A, vector<string> &B) {
-	mp.clear();
-
-	for (string b : B)
-		mp[b]++;
-
-	return partition(A, 0, A.size() - 1);
-}
-
 int main() {
-	vector<vector<int> > M({
-		{ -1, -2}, { -3, -4}
 
-	});
+	// int n ;
+	// cin >> n;
 
-	cout << maximumSumRectangle(2, 2, M);
+
+	cout<<"Num is ";
 }
